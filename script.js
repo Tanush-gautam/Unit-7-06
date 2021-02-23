3006227fd5ea3a3e445e3a3e22d522ea6193d3ed
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-23 15:59:27 +0000

    3.0

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let button = 0
 document.getElementById('results').addEventListener('click', report)
 function report () {
   do {
-    button = prompt('what is my age')
+    button = prompt('what-is-my-age')
     if (age === button) {
-    alert('your right you have gussed my age')
+      alert('your right you have gussed my age')
     } else if (age < button) {
       alert('Your guess was to high my friend')
-    } else if ( age > button) {
+    } else if (age > button) {
       alert('your guess was too low try again')
     }
   } while (age !== button)
